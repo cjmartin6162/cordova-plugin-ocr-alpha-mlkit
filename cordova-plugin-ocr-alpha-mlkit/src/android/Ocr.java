74e5e74b4690714109bd3115bfc1518b10b65ecc
cjmartin6162 <cmartin6162@gmail.com>
2025-05-14 09:17:01 -0700

    Add files via upload

---
+++ cordova-plugin-ocr-alpha-mlkit/src/android/Ocr.java
@@ -0,0 +1,45 @@
+package com.example.ocr;
+
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class Ocr extends CordovaPlugin {
+
+  @Override
+  public boolean execute(String action, JSONArray args, final CallbackContext cb) throws JSONException {
+    if (!"scanImage".equals(action)) return false;
+
+    final String uri = args.getString(0);
+    cordova.getThreadPool().execute(() -> {
+      try {
+        String path = uri.replace("file://", "");
+        Bitmap bmp = BitmapFactory.decodeFile(path);
+        if (bmp == null) {
+          cb.error("Could not load image at " + path);
+          return;
+        }
+
+        InputImage image = InputImage.fromBitmap(bmp, 0);
+        TextRecognizer recognizer = TextRecognition.getClient();
+
+        recognizer.process(image)
+          .addOnSuccessListener(result -> cb.success(result.getText()))
+          .addOnFailureListener(e -> cb.error(e.getMessage()));
+
+      } catch (Exception e) {
+        cb.error(e.getMessage());
+      }
+    });
+
+    return true;
+  }
+}
