07bfb192e9528045ab70fbe84108855f1a0bef65
cjmartin6162 <cmartin6162@gmail.com>
2026-04-14 16:56:47 -0700

    Update Ocr.m
    fix iOS

--- src/ios/Ocr.m
+++ src/ios/Ocr.m
@@ -1,36 +1,47 @@
 #import "Ocr.h"
-   #import <MLKitVision/MLKitVision.h>
-   #import <MLKitTextRecognition/MLKitTextRecognition.h>
-   #import <MLKitTextRecognitionCommon/MLKitTextRecognitionCommon.h>
+#import <Cordova/CDV.h>
+#import <UIKit/UIKit.h>
+#import <GoogleMLKit/TextRecognition.h>
 
 @implementation Ocr
 
 - (void)scanImage:(CDVInvokedUrlCommand*)command {
-  NSString* uri = command.arguments[0];
-  NSString* path = [uri stringByReplacingOccurrencesOfString:@"file://" withString:@""];
-  UIImage* img = [UIImage imageWithContentsOfFile:path];
-  if (!img) {
-    CDVPluginResult* res = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
-                                               messageAsString:@"Could not load image"];
-    [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
-    return;
-  }
-
-  MLKVisionImage* visionImage = [[MLKVisionImage alloc] initWithImage:img];
-  MLKTextRecognizer* recognizer = [MLKTextRecognizer textRecognizer];
-
-  [recognizer processImage:visionImage
-               completion:^(MLKText * _Nullable text, NSError * _Nullable error) {
-    CDVPluginResult* res;
-    if (error || !text) {
-      res = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
-                            messageAsString:error.localizedDescription];
-    } else {
-      res = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
-                            messageAsString:text.text];
+    NSString *uri = command.arguments.count > 0 ? command.arguments[0] : nil;
+    if (uri == nil || ![uri isKindOfClass:[NSString class]] || uri.length == 0) {
+        CDVPluginResult *res = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                                 messageAsString:@"Missing image URI"];
+        [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
+        return;
     }
-    [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
-  }];
+
+    NSString *path = [uri stringByReplacingOccurrencesOfString:@"file://" withString:@""];
+    UIImage *img = [UIImage imageWithContentsOfFile:path];
+
+    if (!img) {
+        CDVPluginResult *res = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                                 messageAsString:@"Could not load image"];
+        [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
+        return;
+    }
+
+    MLKVisionImage *visionImage = [[MLKVisionImage alloc] initWithImage:img];
+    MLKTextRecognizer *recognizer = [MLKTextRecognizer textRecognizer];
+
+    [recognizer processImage:visionImage
+                  completion:^(MLKText * _Nullable text, NSError * _Nullable error) {
+        CDVPluginResult *res = nil;
+
+        if (error || !text) {
+            NSString *message = error.localizedDescription ?: @"No text recognized";
+            res = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                    messageAsString:message];
+        } else {
+            res = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                                    messageAsString:text.text ?: @""];
+        }
+
+        [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
+    }];
 }
 
 @end
